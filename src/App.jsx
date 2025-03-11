a967c53f091ce7f2d31962727f6ade1f29ae7144
Mahipat <59689193+vaghela983@users.noreply.github.com>
2025-03-11 12:56:44 +0530

    add code

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import HomePage from "./components/home_page";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/home_page/index.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>test</Button>
+    </div>
+  );
+};
+export default HomePage;
